37171f3e3dfb8e7b70d9a2bfd37ccac7414f2c0a
shuray <1syuraih@gmail.com>
2022-07-10 22:28:23 +0800

    Done building structure for joysticcs message

--- ArViGa/ArViGa.ino
+++ ArViGa/ArViGa.ino
@@ -2,7 +2,7 @@
 const int buttonPins[16] = {22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37};
 int lastButtState[16] = {};
 const int buttonPrefix = 22;
-
+int buttonState = 0;
 // Joysticks
 const int joystickLXpin = A0; const int joystickLYpin = A1; const int joystickRXpin = A2; const int joystickRYpin = A3;
 // Idle state reads
@@ -11,71 +11,81 @@ int joysticksLXidle = 0; int joysticksLYidle = 0; int joysticksRXidle = 0; int j
 int joysticksLXread = 0; int joysticksLYread = 0; int joysticksRXread = 0; int joysticksRYread = 0;
 /*
      The status integer used to discriminate operations about sending state of;
-        - status = 0x1n : pressed state of any those 16 buttons
-        - status = 0x2n : termination of the pressed state (i.e the button had just been released)
-        - status = 0x3n : right thumbstick
-        - status = 0x4n : left thumbstick
+        - status = 0x9n or 1001 NNNN : pressed state of any those 16 buttons
+        - status = 0x8n or 1000 NNNN : released state of any those 16 butts
+        - status = 0xAn or 1010 NNNN: right thumbstick
+        - status = 0xBn or 1011 NNNN: left thumbstick
         - n is the index of the gamepad starting from 0 and the max is 4
 */
-void sendXinput(byte status, byte button, int axisX, int axisY){
+void sendXinput(byte status, byte buttonByte, int axisX, int axisY){
+    word dataBytesX = 0; word dataBytesY = 0;                      // axisX or Y remapped to word type
+    byte MSBx = 0; byte MSBy = 0; byte LSBx = 0; byte LSBy = 0;    // significant bytes
+    Serial.write(status);
     switch (status >> 4){
-    case 0x1: Serial.write(status); Serial.write(button); break;
-    case 0x2: Serial.write(status); Serial.write(button); break;
-    case 0x3: 
-      Serial.write(status);
-      int dataBytesX = map(axisX, 0, 1023, 0, 65535);
-      int dataBytesY = map(axisY, 0, 1023, 0, 65535);
-      byte MSB = dataBytesX >> 8 ;
-      byte ASB = MSB >> 4;   // Average significant byte
-      byte LSB = dataBytesX << 8;
-    case 0x4: 
-      Serial.write(status);
+    case 0x9: Serial.write(buttonByte); break;
+    case 0x8: Serial.write(buttonByte); break;
+
+    // buttonByte is ignored in both cases below
+    case 0xA:
+      dataBytesX = map(axisX, 0, 1023, 0, 65535); dataBytesY = map(axisY, 0, 1023, 0, 65535);
+      // Literally split dataBytes to Most Significant Bit and Least Significant Bit
+      MSBx = dataBytesX >> 8; LSBx = dataBytesX << 8;
+      MSBy = dataBytesY >> 8; LSBy = dataBytesY << 8;
+
+      // The order of the bytes sent will be important to "feed" the ViGEmClient later by an algorithm
+      Serial.write(MSBx); Serial.write(LSBx); Serial.write(MSBy); Serial.write(LSBy); break;
+    case 0xB:
+      dataBytesX = map(axisX, 0, 1023, 0, 65535); dataBytesY = map(axisY, 0, 1023, 0, 65535);
+      MSBx = dataBytesX >> 8; LSBx = dataBytesX << 8;
+      MSBy = dataBytesY >> 8; LSBy = dataBytesY << 8;
+      Serial.write(MSBx); Serial.write(LSBx); Serial.write(MSBy); Serial.write(LSBy); break;
     }
 }
 
+// All 16 gamepad buttons detection and send xinput signal respectively
 void buttons(){
   for (int i : buttonPins){
-    int buttonState = digitalRead(i);
+    buttonState = digitalRead(i);
     if (buttonState != lastButtState[i-buttonPrefix]){
       if (buttonState == HIGH){
         switch (i){
-          case 22: sendXinput(0x10,0x0,0,0); break;
-          case 23: sendXinput(0x10,0x1,0,0); break;
-          case 24: sendXinput(0x10,0x2,0,0); break;
-          case 25: sendXinput(0x10,0x3,0,0); break;
-          case 26: sendXinput(0x10,0x4,0,0); break;
-          case 27: sendXinput(0x10,0x5,0,0); break;
-          case 28: sendXinput(0x10,0x6,0,0); break;
-          case 29: sendXinput(0x10,0x7,0,0); break;
-          case 30: sendXinput(0x10,0x8,0,0); break;
-          case 31: sendXinput(0x10,0x9,0,0); break;
-          case 32: sendXinput(0x10,0xA,0,0); break;
-          case 33: sendXinput(0x10,0xB,0,0); break;
-          case 34: sendXinput(0x10,0xC,0,0); break;
-          case 35: sendXinput(0x10,0xD,0,0); break;
-          case 36: sendXinput(0x10,0xE,0,0); break;
-          case 37: sendXinput(0x10,0xF,0,0); break;
+          case 22: sendXinput(0x90,0x0,0,0); break;
+          case 23: sendXinput(0x90,0x1,0,0); break;
+          case 24: sendXinput(0x90,0x2,0,0); break;
+          case 25: sendXinput(0x90,0x3,0,0); break;
+          case 26: sendXinput(0x90,0x4,0,0); break;
+          case 27: sendXinput(0x90,0x5,0,0); break;
+          case 28: sendXinput(0x90,0x6,0,0); break;
+          case 29: sendXinput(0x90,0x7,0,0); break;
+          case 30: sendXinput(0x90,0x8,0,0); break;
+          case 31: sendXinput(0x90,0x9,0,0); break;
+          case 32: sendXinput(0x90,0xA,0,0); break;
+          case 33: sendXinput(0x90,0xB,0,0); break;
+          case 34: sendXinput(0x90,0xC,0,0); break;
+          case 35: sendXinput(0x90,0xD,0,0); break;
+          case 36: sendXinput(0x90,0xE,0,0); break;
+          case 37: sendXinput(0x90,0xF,0,0); break;
         }
         lastButtState[i] = HIGH;
       }
     if (buttonState == LOW){
         switch (i){
-          case 22: sendXinput(0x20,0x0,0,0); break;
-          case 23: sendXinput(0x20,0x1,0,0); break;
-          case 24: sendXinput(0x20,0x2,0,0); break;
-          case 25: sendXinput(0x20,0x3,0,0); break;
-          case 26: sendXinput(0x20,0x4,0,0); break;
-          case 27: sendXinput(0x20,0x5,0,0); break;
-          case 28: sendXinput(0x20,0x6,0,0); break;
-          case 29: sendXinput(0x20,0x7,0,0); break;
-          case 30: sendXinput(0x20,0x8,0,0); break;
-          case 31: sendXinput(0x20,0x9,0,0); break;
-          case 32: sendXinput(0x20,0xA,0,0); break;
-          case 33: sendXinput(0x20,0xB,0,0); break;
-          case 34: sendXinput(0x20,0xC,0,0); break;
-          case 35: sendXinput(0x20,0xD,0,0); break;
-          case 36: sendXinput(0x20,0xE,0,0); break;
-          case 37: sendXinput(0x20,0xF,0,0); break;
+          case 22: sendXinput(0x80,0x0,0,0); break;
+          case 23: sendXinput(0x80,0x1,0,0); break;
+          case 24: sendXinput(0x80,0x2,0,0); break;
+          case 25: sendXinput(0x80,0x3,0,0); break;
+          case 26: sendXinput(0x80,0x4,0,0); break;
+          case 27: sendXinput(0x80,0x5,0,0); break;
+          case 28: sendXinput(0x80,0x6,0,0); break;
+          case 29: sendXinput(0x80,0x7,0,0); break;
+          case 30: sendXinput(0x80,0x8,0,0); break;
+          case 31: sendXinput(0x80,0x9,0,0); break;
+          case 32: sendXinput(0x80,0xA,0,0); break;
+          case 33: sendXinput(0x80,0xB,0,0); break;
+          case 34: sendXinput(0x80,0xC,0,0); break;
+          case 35: sendXinput(0x80,0xD,0,0); break;
+          case 36: sendXinput(0x80,0xE,0,0); break;
+          case 37: sendXinput(0x80,0xF,0,0); break;
         }
         lastButtState[i] = LOW;
       }
@@ -94,10 +104,12 @@ void joysticcs(){
   joysticksRXread = analogRead(joystickRXpin); joysticksRYread = analogRead(joystickRYpin);
   joysticksLXread = analogRead(joystickLXpin); joysticksLYread = analogRead(joystickLYpin);
   if (checkActiveAnalog(joysticksRXread, joysticksRXidle) || checkActiveAnalog(joysticksRYread, joysticksRYidle)){
-    // Byte of 0x14 to 0x19 dont't have any meaning since the max gamepad index is 0x13
-    sendXinput(0x40, 0x14, )
+    // The 2nd byte (buttonByte parameter) will not be used by the function
+    sendXinput(0xA0, 0, joysticksRXread, joysticksRYread);
   }
-  if (checkActiveAnalog(joysticksLXread, joysticksLXidle) || checkActiveAnalog(joysticksLYread, joysticksLYidle)){}
+  if (checkActiveAnalog(joysticksLXread, joysticksLXidle) || checkActiveAnalog(joysticksLYread, joysticksLYidle)){
+    sendXinput(0xB0, 0, joysticksLXread, joysticksLYread);
+  }
 }
 
 void setup() {
@@ -109,7 +121,6 @@ void setup() {
     lastButtState[i] = LOW;
   }
 
-  int i = 0;
   while (millis() < 2000){
     if (millis() % 200 == 0){
       joysticksLXidle += analogRead(joystickLXpin); joysticksLYidle += analogRead(joystickLYpin);
@@ -123,5 +134,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  buttons();
+  joysticcs();
 }
